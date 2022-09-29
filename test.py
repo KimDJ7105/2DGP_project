73b8de15ac5f209468d0b4aa485fd785f97ad639
KimDJ7105 <rlaehdwo1225@naver.com>
2022-09-29 22:49:29 +0900

    KeyCommand added
    키보드 명령 추가 (wasd, ESC)

--- test.py
+++ test.py
@@ -17,30 +17,72 @@ def print_map(  type ) :
 
 def set_player(type) :
     global player
+    global frame
     if type == 0 :
         player = load_image('player_resource/Player_Standing.png')
+        player.draw(x,y)
     elif type == 1 :
         player = load_image('player_resource/wark.png')
+        player.clip_draw(frame * 100 , 0 ,100, 100,x,y)
     elif type == 2 :
         player = load_image('player_resource/jump.png')
+        player.draw(x,y)
+
+def Handle_events() :
+    global x
+    global y
+    global running
+    global dir
+    global pl_draw
+    events = get_events()
+    for event in events :
+        if event.type == SDL_QUIT :
+            running = False
+        elif event.type == SDL_KEYDOWN :
+            if event.key == SDLK_ESCAPE :
+                running = False
+            elif event.key == SDLK_d :
+                dir = 1
+                pl_draw = 1
+            elif event.key == SDLK_a :
+                dir = -1
+                pl_draw = 1
+            elif event.key == SDLK_w :
+                y = 140
+                pl_draw = 2
+        elif event.type == SDL_KEYUP :
+            if event.key == SDLK_d :
+                dir = 0
+                pl_draw = 0
+            elif event.key == SDLK_a :
+                dir = 0
+                pl_draw = 0
+            elif event.key == SDLK_w :
+                y = 90
+                pl_draw = 0
+
 
 frame = 0;
+x = 800 // 2
+y = 90
+dir = 0
+running = True
+pl_draw = 0
+map_draw = 0
 
-for x in range(40,600 + 1, 10) :
+while running :
     clear_canvas()
-    print_map(0)
-    set_player(1)
-    player.clip_draw(0 + (frame * 100),0,100,100,x,90)
+    print_map(map_draw)
+    set_player(pl_draw)
     update_canvas()
-    frame = (frame + 1) % 4
-    if frame == 3 :
-        for y in range(90,120+1, 10) :
-            set_player(2)
-            player.draw(x,y)
-            delay(0.1)
-    delay(0.05)
-    get_events()
 
+    Handle_events()
+    frame = (frame + 1) % 4
+    if x < 800 and dir == 1 :
+        x += dir * 5
+    if x > 0 and dir == -1 :
+        x += dir * 5
+    delay(0.01)
 
 
 close_canvas()
