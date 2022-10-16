4c2bf4bf155b9aed31f0747057443f5f3dd42842
KimDJ7105 <rlaehdwo1225@naver.com>
2022-10-16 22:53:17 +0900

    rolling added

--- classes/player_class.py
+++ classes/player_class.py
@@ -12,45 +12,64 @@ class Player :
         self.last_dir = 0
         self.line = 5 #sprite line
         self.jump = False
+        self.role = False
+        self.role_count = 0
 
     def draw(self) :
         self.sprite.clip_draw(self.frame * 100, self.line * 100 ,100, 100, self.x, self.y)
 
     def move(self) :
-        if self.jump == False : #not jump or in mid air
-            if self.y < 90 :
-                self.y = 90
-            elif self.y > 90 :
-                self.y -= 2
-                if self.y == 90 :
-                    self.set_dir(self.last_dir)
-                    self.set_dir(0)
-                else :
+        if self.role == True :
+            if self.last_dir == 0 or self.last_dir == 1 :
+                if self.x < 800 and self.role_count == 10:
+                    self.x += 30
+            elif self.last_dir == -1 :
+                if self.x > 0 and self.role_count == 10 :
+                    self.x -= 30
+            if self.frame == 3 :
+                self.role = False
+                self.set_dir(self.last_dir)
+                self.set_dir(0)
+        elif self.role == False :
+            if self.jump == False : #not jump or in mid air
+                if self.y < 90 :
+                    self.y = 90
+                elif self.y > 90 :
+                    self.y -= 2
+                    if self.y == 90 :
+                        self.set_dir(self.last_dir)
+                        self.set_dir(0)
+                    else :
+                        if self.last_dir == 1 or self.last_dir == 0 :
+                            self.line = 1
+                            self.frame = 0
+                        elif self.last_dir == -1 :
+                            self.line = 0
+                            self.frame = 0
+            elif self.jump == True : #jumping
+                if self.y < 160 :
+                    self.y += 2
                     if self.last_dir == 1 or self.last_dir == 0 :
                         self.line = 1
                         self.frame = 0
                     elif self.last_dir == -1 :
                         self.line = 0
                         self.frame = 0
-        elif self.jump == True : #jumping
-            if self.y < 160 :
-                self.y += 2
-                if self.last_dir == 1 or self.last_dir == 0 :
-                    self.line = 1
-                    self.frame = 0
-                elif self.last_dir == -1 :
-                    self.line = 0
-                    self.frame = 0
-            elif self.y >= 160 :
-                self.jump = False
-        if self.dir == -1 and self.x > 0 or self.dir == 1 and self.x < 800: #left or right
-            self.x += 2* self.dir
+                elif self.y >= 160 :
+                    self.jump = False
+            if self.dir == -1 and self.x > 0 or self.dir == 1 and self.x < 800: #left or right
+                self.x += 2* self.dir
 
     def frame_update(self) :
         if self.line == 2 or self.line == 3 :
             self.frame = (self.frame + 1) % 7
-        elif self.line == 4 or self.line == 5 :
+        elif self.line == 4 or self.line == 5 and self.role == False:
             self.frame = (self.frame + 1) % 4
+        elif self.role == True :
+            self.role_count += 1
+            if self.role_count == 11 :
+                self.frame += 1
+                self.role_count = 0
 
     def set_line(self,num) :
         self.line = num
@@ -69,4 +88,14 @@ class Player :
                 self.set_line(5)
 
     def set_jump(self, jump_bool) :
-        self.jump = jump_bool
+        if self.role == False and self.y == 90:
+            self.jump = jump_bool
+
+    def set_role(self, role_bool) :
+        if self.role == False :
+            self.role = role_bool
+            self.frame = 1
+            if self.last_dir == 0 or self.last_dir == 1:
+                self.line = 1
+            elif self.last_dir == -1 :
+                self.line = 0
--- states/play_state.py
+++ states/play_state.py
@@ -29,15 +29,14 @@ def draw():
     map.draw()
     cat.draw()
     update_canvas()
-    pass
+
 
 def handle_events():
     events = get_events()
     for event in events :
         if event.type == SDL_KEYDOWN :
             if event.key == SDLK_w :
-                if cat.y == 90 :
-                    cat.set_jump(True)
+                cat.set_jump(True)
             elif event.key == SDLK_a :
                 cat.set_dir(-1)
             elif event.key == SDLK_s :
@@ -45,7 +44,7 @@ def handle_events():
             elif event.key == SDLK_d :
                 cat.set_dir(1)
             elif event.key == SDLK_SPACE :
-                pass
+                cat.set_role(True)
             elif event.key == SDLK_m :
                 game_framework.push_state(map_select_state)
         elif event.type == SDL_KEYUP :
