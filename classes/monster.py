da0e1fd22fb583f42089d99320447f2068f1a30e
KimDJ7105 <rlaehdwo1225@naver.com>
2022-10-17 14:33:06 +0900

    monster Parent class added

---
+++ classes/monster.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+class Monster :
+    #sprite = load_image('')
+    hp = 100
+    atk_count = 0
+    x = 400
+    y = 300
+
+    def attack_far(self) :
+        pass
+
+    def attack_close(self) :
+        pass
+
+    def attack_mid(self) :
+        pass
