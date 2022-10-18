36c7ac11c9640c01e9ebf055978b1233122a3ee0
KimDJ7105 <rlaehdwo1225@naver.com>
2022-10-18 13:23:40 +0900

    player pos init when map changed

--- states/map_select_state.py
+++ states/map_select_state.py
@@ -19,7 +19,7 @@ def update() :
 
 def draw() :
     clear_canvas()
-    play_state.map.draw()
+    play_state.map.draw_wide()
     play_state.cat.draw()
     map_select.draw(400,300, 400,300)
     update_canvas()
@@ -31,15 +31,19 @@ def handle_events():
         if event.type == SDL_KEYDOWN :
             if event.key == SDLK_1 :
                 play_state.map.set_map_type(1)
+                play_state.cat.x = 400
                 game_framework.pop_state()
             elif event.key == SDLK_2 :
                 play_state.map.set_map_type(2)
+                play_state.cat.x = 400
                 game_framework.pop_state()
             elif event.key == SDLK_3 :
                 play_state.map.set_map_type(3)
+                play_state.cat.x = 400
                 game_framework.pop_state()
             elif event.key == SDLK_0 :
                 play_state.map.set_map_type(0)
+                play_state.cat.x = 400
                 game_framework.pop_state()
         elif event.type == SDL_QUIT :
             game_framework.quit()
