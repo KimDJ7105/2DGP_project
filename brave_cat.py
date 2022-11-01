b464c0a52b7c758f3615b6babc6e079968eb0d5c
KimDJ7105 <rlaehdwo1225@naver.com>
2022-11-02 00:12:45 +0900

    for debug

--- brave_cat.py
+++ brave_cat.py
@@ -1,11 +1,12 @@
 from pico2d import *
 import game_framework
 import states.title_state as title_state
+import states.play_state as play_state
 
 open_canvas()
 
 hide_cursor()
 
-game_framework.run(title_state)
+game_framework.run(play_state)
 
 close_canvas()
