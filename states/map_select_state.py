b6b77040cb6169e1e5107eb5bb1941d4fb86b187
KimDJ7105 <rlaehdwo1225@naver.com>
2022-11-08 13:26:37 +0900

    temp spawn volcano boss

--- states/map_select_state.py
+++ states/map_select_state.py
@@ -1,6 +1,7 @@
 from pico2d import *
 import states.play_state as play_state
 import game_framework
+import classes.monster_class as Boss
 
 map_select = None
 
@@ -29,21 +30,23 @@ def handle_events():
     events = get_events()
     for event in events :
         if event.type == SDL_KEYDOWN :
-            if event.key == SDLK_1 :
+            if event.key == SDLK_1 : #
                 play_state.map.set_map_type(1)
                 play_state.cat.x = 400
                 game_framework.pop_state()
-            elif event.key == SDLK_2 :
+            elif event.key == SDLK_2 : #
                 play_state.map.set_map_type(2)
                 play_state.cat.x = 400
                 game_framework.pop_state()
-            elif event.key == SDLK_3 :
+            elif event.key == SDLK_3 : #volcano
                 play_state.map.set_map_type(3)
                 play_state.cat.x = 400
+                play_state.boss = Boss.Monster()
                 game_framework.pop_state()
             elif event.key == SDLK_0 :
                 play_state.map.set_map_type(0)
                 play_state.cat.x = 400
+                play_state.boss = None
                 game_framework.pop_state()
         elif event.type == SDL_QUIT :
             game_framework.quit()
