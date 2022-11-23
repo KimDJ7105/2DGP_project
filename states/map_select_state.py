c426fc3a3fcaf0a4c31ef82f980439c9672855fa
KimDJ7105 <rlaehdwo1225@naver.com>
2022-11-23 19:05:29 +0900

    map select state is ammended
    for timer add

--- states/map_select_state.py
+++ states/map_select_state.py
@@ -27,25 +27,26 @@ def draw() :
 
 
 def handle_events():
+    play_state.cat.x = 1000
     events = get_events()
     for event in events :
         if event.type == SDL_KEYDOWN :
             if event.key == SDLK_1 : #
                 play_state.map.set_map_type(1)
-                play_state.cat.x = 1000
+                play_state.timer = get_time()
                 game_framework.pop_state()
             elif event.key == SDLK_2 : #desert
                 play_state.map.set_map_type(2)
-                play_state.cat.x = 1000
+                play_state.timer = get_time()
                 game_world.spawn = True
                 game_framework.pop_state()
             elif event.key == SDLK_3 : #volcano
                 play_state.map.set_map_type(3)
-                play_state.cat.x = 1000
+                play_state.timer = get_time()
                 game_framework.pop_state()
             elif event.key == SDLK_0 :
                 play_state.map.set_map_type(0)
-                play_state.cat.x = 1000
+                play_state.timer = get_time()
                 game_framework.pop_state()
         elif event.type == SDL_QUIT :
             game_framework.quit()
